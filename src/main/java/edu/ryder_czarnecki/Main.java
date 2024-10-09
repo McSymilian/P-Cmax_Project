7a3026c3b872eb61075acbbea91db4cf7a4bf4dc
Maksymilian Ryder <77546701+McSymilian@users.noreply.github.com>
2024-10-09 21:46:12 +0200

    prezentacja rozwiązania

--- src/main/java/edu/ryder_czarnecki/Main.java
+++ src/main/java/edu/ryder_czarnecki/Main.java
@@ -1,7 +1,27 @@
 package edu.ryder_czarnecki;
 
+import edu.ryder_czarnecki.process.Process;
+import edu.ryder_czarnecki.process_manager.ProcessManager;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        List<Integer> input = List.of(4, 3, 4, 4, 2, 1, 1);
+
+        ProcessManager processManager = ProcessManager
+                .builder()
+                .processorsCount(3)
+                .build()
+                .addProcesses(input
+                        .stream()
+                        .map(Process::new)
+                        .toList()
+                );
+
+        int res = processManager.getCMax();
+
+        System.out.println(res);
+        System.out.println(processManager.prettyPrint());
     }
 }
