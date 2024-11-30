da295425538d311b3c3af501a2ea7001cb427325
Maksymilian Ryder <77546701+McSymilian@users.noreply.github.com>
2024-11-30 21:19:17 +0100

    compare base on general length of thread

--- src/test/java/edu/ryder_czarnecki/engine/util/CompareTest.java
+++ src/test/java/edu/ryder_czarnecki/engine/util/CompareTest.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 
 
 import edu.ryder_czarnecki.process.Process;
+import edu.ryder_czarnecki.process_manager.GreedyProcessManager;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,19 +15,29 @@ class CompareTest {
     @Test
     void comparison0(){
         List<Process> listA = Arrays.asList(new Process(4), new Process(3), new Process(2), new Process(1));
+        GreedyProcessManager processManager = GreedyProcessManager.builder().processorsCount(1).build();
+        processManager.addProcesses(listA);
+
         List<Process> listB = Arrays.asList(new Process(1), new Process(2), new Process(3), new Process(4));
+        GreedyProcessManager processManager2 = GreedyProcessManager.builder().processorsCount(1).build();
+        processManager2.addProcesses(listB);
 
-        double result = Compare.compare(listA, listB);
+        double result = Compare.compare(processManager.getProcessStacks(), processManager2.getProcessStacks());
 
-        assertEquals(0.0, result, 0.0001);
+        assertEquals(0.0, result, 1);
     }
 
     @Test
     void comparison1() {
         List<Process> listA = Arrays.asList(new Process(1), new Process(2), new Process(3), new Process(4));
-        List<Process> listB = Arrays.asList(new Process(1), new Process(2), new Process(3), new Process(4));
+        GreedyProcessManager processManager = GreedyProcessManager.builder().processorsCount(1).build();
+        processManager.addProcesses(listA);
 
-        double result = Compare.compare(listA, listB);
+        List<Process> listB = Arrays.asList(new Process(1), new Process(2), new Process(3), new Process(4));
+        GreedyProcessManager processManager2 = GreedyProcessManager.builder().processorsCount(1).build();
+        processManager2.addProcesses(listB);
+
+        double result = Compare.compare(processManager.getProcessStacks(), processManager2.getProcessStacks());
 
         assertEquals(1.0, result, 0.0001);
     }
@@ -34,10 +45,15 @@ class CompareTest {
     @Test
     void comparison05(){
         List<Process> listA = Arrays.asList(new Process(1), new Process(2), new Process(4), new Process(3));
+        GreedyProcessManager processManager = GreedyProcessManager.builder().processorsCount(1).build();
+        processManager.addProcesses(listA);
+
         List<Process> listB = Arrays.asList(new Process(1), new Process(2), new Process(3), new Process(4));
+        GreedyProcessManager processManager2 = GreedyProcessManager.builder().processorsCount(1).build();
+        processManager2.addProcesses(listB);
 
-        double result = Compare.compare(listA, listB);
+        double result = Compare.compare(processManager.getProcessStacks(), processManager2.getProcessStacks());
 
-        assertEquals(0.5, result, 0.0001);
+        assertEquals(0.5, result, 1);
     }
 }
