724d4565a685de21eaec0bb1b38d70a1d7a77d9a
Maksymilian Ryder <77546701+McSymilian@users.noreply.github.com>
2024-11-30 20:38:18 +0000

    minimal changes

--- src/main/java/edu/ryder_czarnecki/engine/generational_setup/CustomGenerationalSetup.java
+++ src/main/java/edu/ryder_czarnecki/engine/generational_setup/CustomGenerationalSetup.java
@@ -7,18 +7,17 @@ import java.util.concurrent.TimeUnit;
 
 @Builder
 public class CustomGenerationalSetup {
-    int maxGenerations;
-    int generationSize;
+    @Range(from = 1, to = Integer.MAX_VALUE) int maxGenerations;
+    @Range(from = 1, to = Integer.MAX_VALUE) int generationSize;
     @Range(from = 0, to = 1) double mutationIntensity;
     @Range(from = 0, to = 1) double crossoverIntensity;
     @Range(from = 0, to = 1) double minimalDiversity;
     @Range(from = 0, to = 1) double mutationPart;
     @Range(from = 0, to = 1) double randomPart;
-    long maxGenerationTime;
+    @Range(from = 1, to = Long.MAX_VALUE) long maxGenerationTime;
     TimeUnit maxGenerationTimeUnit;
 
     public GenerationalSetup parseIntoRecord() {
-        validateRanges();
         return new GenerationalSetup(
                 maxGenerations,
                 generationSize,
@@ -30,26 +29,5 @@ public class CustomGenerationalSetup {
                 maxGenerationTime,
                 maxGenerationTimeUnit
         );
-    }
-
-    private void validateRanges() {
-        if (mutationIntensity < 0 || mutationIntensity > 1) {
-            throw new IllegalArgumentException("mutationIntensity must be in range [0, 1]");
-        }
-        if (crossoverIntensity < 0 || crossoverIntensity > 1) {
-            throw new IllegalArgumentException("crossoverIntensity must be in range [0, 1]");
-        }
-        if (minimalDiversity < 0 || minimalDiversity > 1) {
-            throw new IllegalArgumentException("minimalDiversity must be in range [0, 1]");
-        }
-        if (mutationPart < 0 || mutationPart > 1) {
-            throw new IllegalArgumentException("mutationPart must be in range [0, 1]");
-        }
-        if (randomPart < 0 || randomPart > 1) {
-            throw new IllegalArgumentException("randomPart must be in range [0, 1]");
-        }
-        if (maxGenerationTime <= 0) {
-            throw new IllegalArgumentException("maxGenerationTime must be non-negative");
-        }
     }
 }
--- src/main/java/edu/ryder_czarnecki/engine/generational_setup/FileSourcedGenerationalSetup.java
+++ src/main/java/edu/ryder_czarnecki/engine/generational_setup/FileSourcedGenerationalSetup.java
@@ -9,18 +9,18 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class FileSourcedGenerationalSetup{
-    int maxGenerations;
-    int generationSize;
+    @Range(from = 1, to = Integer.MAX_VALUE) int maxGenerations;
+    @Range(from = 1, to = Integer.MAX_VALUE) int generationSize;
     @Range(from = 0, to = 1) double mutationIntensity;
     @Range(from = 0, to = 1) double crossoverIntensity;
     @Range(from = 0, to = 1) double minimalDiversity;
     @Range(from = 0, to = 1) double mutationPart;
     @Range(from = 0, to = 1) double randomPart;
-    long maxGenerationTime;
+    @Range(from = 1, to = Long.MAX_VALUE) long maxGenerationTime;
     TimeUnit maxGenerationTimeUnit;
     String separator;
 
-    private static final Set<String> REQUIERD_KEYS = Set.of("maxGenerations", "generationSize", "mutationIntensity", "crossoverIntensity", "minimalDiversity", "mutationPart", "randomPart", "maxGenerationTime", "maxGenerationTimeUnit");
+    private static final Set<String> REQUIERED_KEYS = Set.of("maxGenerations", "generationSize", "mutationIntensity", "crossoverIntensity", "minimalDiversity", "mutationPart", "randomPart", "maxGenerationTime", "maxGenerationTimeUnit");
     private static final Set<String> activatedKeys= new HashSet<>();
 
     public FileSourcedGenerationalSetup separator(String sep){
@@ -107,7 +107,6 @@ public class FileSourcedGenerationalSetup{
     }
 
     public GenerationalSetup parseIntoRecord() {
-        validateRanges();
         return new GenerationalSetup(
                 maxGenerations,
                 generationSize,
@@ -120,26 +119,4 @@ public class FileSourcedGenerationalSetup{
                 maxGenerationTimeUnit
         );
     }
-
-    private void validateRanges() {
-        if (mutationIntensity < 0 || mutationIntensity > 1) {
-            throw new IllegalArgumentException("mutationIntensity must be in range [0, 1]");
-        }
-        if (crossoverIntensity < 0 || crossoverIntensity > 1) {
-            throw new IllegalArgumentException("crossoverIntensity must be in range [0, 1]");
-        }
-        if (minimalDiversity < 0 || minimalDiversity > 1) {
-            throw new IllegalArgumentException("minimalDiversity must be in range [0, 1]");
-        }
-        if (mutationPart < 0 || mutationPart > 1) {
-            throw new IllegalArgumentException("mutationPart must be in range [0, 1]");
-        }
-        if (randomPart < 0 || randomPart > 1) {
-            throw new IllegalArgumentException("randomPart must be in range [0, 1]");
-        }
-        if (maxGenerationTime <= 0) {
-            throw new IllegalArgumentException("maxGenerationTime must be non-negative");
-        }
-    }
-
 }
